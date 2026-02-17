d5c6cc34d3f216511dfb62463602995ccbaa1396
Harsh Modi <harshmodi1703@gmail.com>
2026-02-17 21:10:55 +0530

    rename files to jsx for vite

---
+++ vite.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+  },
+})
